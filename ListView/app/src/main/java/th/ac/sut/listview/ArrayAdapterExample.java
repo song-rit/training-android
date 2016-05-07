40e1b9f74675a7da459cc9ac6386b701751d419c
nirvana-1993 <songojame@gmail.com>
2016-05-07 08:57:10 +0100

    training ArrayAdapter

---
+++ ListView/app/src/main/java/th/ac/sut/listview/ArrayAdapterExample.java
@@ -0,0 +1,54 @@
+package th.ac.sut.listview;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ArrayAdapterExample extends AppCompatActivity {
+
+    private ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.array_adapter_example);
+
+        infixView();
+
+        final String[] family = {"Jame", "Jib", "Jo"};
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, family){
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textView = (TextView) view;
+                textView.setTextColor(Color.RED);
+                return view;
+            }
+        };
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView textView = (TextView) view;
+                textView.setTextColor(Color.GREEN);
+                Log.d("View", textView.getText().toString());
+                Toast.makeText(getApplicationContext(), textView.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void infixView() {
+        listView = (ListView) findViewById(R.id.listView);
+    }
+}
