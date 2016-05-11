1db57daf9da1b7ffbf5ae13fdb8d1062dd169b75
Songrit Maleerat <song.cpe@gmail.com>
2016-05-11 10:22:41 +0700

    update Spinner

--- Spinner/app/src/main/java/th/ac/sut/spinner/SpinnerExample.java
+++ Spinner/app/src/main/java/th/ac/sut/spinner/SpinnerExample.java
@@ -2,14 +2,55 @@ package th.ac.sut.spinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SpinnerExample extends AppCompatActivity {
+
+    private Spinner spinnerYear;
+    private Spinner spinnerTerm;
+
+    private List<Integer> year = new ArrayList<>();
+    private List<Integer> term = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spinner_example);
 
-        
+        infixView();
+        createYearData();
+        createTermData();
+
+        ArrayAdapter<Integer> adapterYear = new ArrayAdapter<Integer>(SpinnerExample.this, R.layout.support_simple_spinner_dropdown_item, year);
+        spinnerYear.setAdapter(adapterYear);
+
+        ArrayAdapter<Integer> adapterTerm = new ArrayAdapter<Integer>(SpinnerExample.this, R.layout.support_simple_spinner_dropdown_item, term);
+        spinnerTerm.setAdapter(adapterTerm);
+    }
+
+    private void createTermData() {
+
+        term.add(3);
+        term.add(2);
+        term.add(1);
+    }
+
+    private void createYearData() {
+
+        year.add(2559);
+        year.add(2558);
+        year.add(2557);
+        year.add(2556);
+        year.add(2555);
+    }
+
+
+    private void infixView() {
+        spinnerTerm = (Spinner) findViewById(R.id.spinner_term);
+        spinnerYear = (Spinner) findViewById(R.id.spinner_year);
     }
 }
