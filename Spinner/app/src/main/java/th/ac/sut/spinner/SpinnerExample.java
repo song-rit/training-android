b6e64b040c7841ace77bf37fb27c67a045a6ca5c
Songrit Maleerat <song.cpe@gmail.com>
2016-05-11 09:27:04 +0700

    add Spinner

---
+++ Spinner/app/src/main/java/th/ac/sut/spinner/SpinnerExample.java
@@ -0,0 +1,15 @@
+package th.ac.sut.spinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpinnerExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner_example);
+
+        
+    }
+}
