48cb1b9aacbd43d01cb0d759478189b2a4923480
Songrit Maleerat <song.cpe@gmail.com>
2016-05-13 14:47:19 +0700

    add layout in view

--- RelativeLayout/app/src/main/java/th/ac/sut/relativelayout/MainActivity.java
+++ RelativeLayout/app/src/main/java/th/ac/sut/relativelayout/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout);
+        setContentView(R.layout.layout_in_parent_view);
     }
 }
