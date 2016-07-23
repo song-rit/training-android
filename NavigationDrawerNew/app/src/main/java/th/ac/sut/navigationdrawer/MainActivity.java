a2bb3e9e8a14557fe541eb258c98cda4b271c072
songojame <song.cpe@gmail.com>
2016-07-23 17:25:45 +0700

    update

--- NavigationDrawerNew/app/src/main/java/th/ac/sut/navigationdrawer/MainActivity.java
+++ NavigationDrawerNew/app/src/main/java/th/ac/sut/navigationdrawer/MainActivity.java
@@ -1,20 +1,25 @@
 package th.ac.sut.navigationdrawer;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     ActionBarDrawerToggle toggle;
     DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         infixView();
 
@@ -22,13 +27,14 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(this);
+
 
     }
 
     private void infixView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
     }
 
     @Override
@@ -40,5 +46,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_new:
+                Toast.makeText(MainActivity.this, "New", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_delete:
+                Toast.makeText(MainActivity.this, "Delete", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_setting:
+                Toast.makeText(MainActivity.this, "Setting", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        if(drawerLayout == null) {
+            drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        }
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+
+        return false;
     }
 }
