f365987e200b2c7fdbe0bdaf6eec2f80ea6c0e67
songojame <song.cpe@gmail.com>
2016-07-23 15:06:21 +0700

    Toolbar

--- Toolbar/app/src/main/java/th/ac/sut/toolbar/MainActivity.java
+++ Toolbar/app/src/main/java/th/ac/sut/toolbar/MainActivity.java
@@ -1,13 +1,20 @@
 package th.ac.sut.toolbar;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +24,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        infixView();
+
         getSupportActionBar().setTitle(R.string.app_title);
         getSupportActionBar().setSubtitle(R.string.app_sub_title);
         getSupportActionBar().setIcon(R.drawable.ic_star);
+
+//        Intent searchIntent = getIntent();
+//        if(Intent.ACTION_SEARCH.equals(searchIntent.getAction())) {
+//
+//            String query = searchIntent.getStringExtra(SearchManager.QUERY);
+//            Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+//        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.countries));
+        listView.setAdapter(adapter);
+    }
+
+    private void infixView() {
+        listView = (ListView) findViewById(R.id.list_view_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
         return super.onCreateOptionsMenu(menu);
     }
 
---
+++ Toolbar/app/src/main/java/th/ac/sut/toolbar/SearchResultsActivity.java
@@ -0,0 +1,56 @@
+package th.ac.sut.toolbar;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+
+        infixView();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.search_result_toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(R.string.app_result_tittle);
+        getSupportActionBar().setSubtitle(R.string.app_result_sub_title);
+        getSupportActionBar().setIcon(R.drawable.ic_star);
+
+        String query = new String();
+
+        Intent searchIntent = getIntent();
+        if(Intent.ACTION_SEARCH.equals(searchIntent.getAction())) {
+
+            query = searchIntent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(SearchResultsActivity.this, query, Toast.LENGTH_SHORT).show();
+        }
+
+        String[] countries = getResources().getStringArray(R.array.countries);
+        ArrayList<String> searchResults = new ArrayList<String>();
+        for(int i = 0; i < countries.length; i++) {
+            if(countries[i].toLowerCase().contains(query.toLowerCase())) {
+                searchResults.add(countries[i]);
+            }
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, searchResults);
+        listView.setAdapter(adapter);
+    }
+
+    private void infixView() {
+        listView = (ListView) findViewById(R.id.list_view_result_search);
+    }
+}
