9a8a83298af12c56338d4b9177e3e812fbf1221e
songojame <song.cpe@gmail.com>
2016-07-28 17:20:29 +0700

    add EditText

---
+++ EditText/app/src/main/java/th/ac/sut/edittext/MainActivity.java
@@ -0,0 +1,15 @@
+package th.ac.sut.edittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
