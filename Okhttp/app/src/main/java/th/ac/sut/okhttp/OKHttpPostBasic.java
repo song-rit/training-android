b54567e4614e0fc07fe142effce390f62ce2fe7f
Songrit Maleerat <song.cpe@gmail.com>
2016-08-15 16:55:03 +0700

    update HTTPGet method

--- Okhttp/app/src/main/java/th/ac/sut/okhttp/OKHttpPostBasic.java
+++ Okhttp/app/src/main/java/th/ac/sut/okhttp/OKHttpPostBasic.java
@@ -1,14 +1,77 @@
 package th.ac.sut.okhttp;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class OKHttpPostBasic extends AppCompatActivity {
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    OkHttpClient client = new OkHttpClient();
+    private String url = "http://www.roundsapp.com/post";
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try(Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    String bowlingJson(String player1, String player2) {
+        return "{'winCondition':'HIGH_SCORE',"
+                + "'name':'Bowling',"
+                + "'round':4,"
+                + "'lastSaved':1367702411696,"
+                + "'dateStarted':1367702378785,"
+                + "'players':["
+                + "{'name':'" + player1 + "','history':[10,8,6,7,8],'color':-13388315,'total':39},"
+                + "{'name':'" + player2 + "','history':[6,10,5,10,10],'color':-48060,'total':41}"
+                + "]}";
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-        
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                OKHttpPostBasic postBasic = new OKHttpPostBasic();
+                String json = postBasic.bowlingJson("Jib", "Jo");
+                try {
+                    final String response = postBasic.post(url, json);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+//                            Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        thread.run();
+
+    }
 }
--- Okhttp/app/src/main/java/th/ac/sut/okhttp/OkHttpGetBasic.java
+++ Okhttp/app/src/main/java/th/ac/sut/okhttp/OkHttpGetBasic.java
@@ -1,13 +1,13 @@
 package th.ac.sut.okhttp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import java.io.IOException;
 
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -16,50 +16,45 @@ import okhttp3.Response;
 public class OkHttpGetBasic extends AppCompatActivity {
 
     String url = "https://docs.google.com/uc?authuser=0&id=0B_IchW5V8GCWMEV4X044Tm5OQkk&export=download";
-
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_okhttp_basic);
-    }
-
-    public String HTTPGetBuiltIn(String url) {
-        final StringBuffer responseString = new StringBuffer("");
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(url).build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-            }
-
-            @Override
-            public void onResponse(Call call, final Response response) throws IOException {
-                responseString.append(response.body().string());
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(OkHttpGetBasic.this, responseString.toString(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-        return responseString.toString();
-    }
-
-    public void HTTPGetMyThread() {
         Thread thread = new Thread() {
-
             @Override
             public void run() {
                 super.run();
-
-                OkHttpClient client = new OkHttpClient();
-                Request request = new Request.Builder().url(url).build();
-
                 try {
-                    Response response = client.newCall(request).execute();
-                    final String responseString = response.body().string();
+                    final String reposeString = HTTPGet2(url);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(OkHttpGetBasic.this, reposeString, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+        HTTPGet(url);
+    }
+
+    public void HTTPGet(final String url) {
+        final StringBuffer responseString = new StringBuffer("");
+        Thread thread = new Thread() {
+            @TargetApi(Build.VERSION_CODES.KITKAT)
+            @Override
+            public void run() {
+                super.run();
+                OkHttpClient client = new OkHttpClient();
+
+                Request request = new Request.Builder().url(url).build();
+                try (Response response = client.newCall(request).execute()) {
+                    responseString.append(response.body().string());
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -69,11 +64,20 @@ public class OkHttpGetBasic extends AppCompatActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
             }
         };
-
         thread.start();
     }
 
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    String HTTPGet2(final String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url(url).build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
 
 }
