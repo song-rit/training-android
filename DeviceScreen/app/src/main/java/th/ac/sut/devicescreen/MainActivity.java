1093fc3e47af82ea4ebf60a52d2e8b0f3489da38
songojame <song.cpe@gmail.com>
2016-08-16 17:41:32 +0700

    update

--- DeviceScreen/app/src/main/java/th/ac/sut/devicescreen/MainActivity.java
+++ DeviceScreen/app/src/main/java/th/ac/sut/devicescreen/MainActivity.java
@@ -2,12 +2,27 @@ package th.ac.sut.devicescreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resolution;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        infixView();
+
+        int screenWidth = getWindowManager().getDefaultDisplay().getWidth();
+        int screenHeight = getWindowManager().getDefaultDisplay().getHeight();
+        resolution.setText(String.valueOf(screenWidth) + " x " + String.valueOf(screenHeight));
+
+
+    }
+
+    private void infixView() {
+        resolution = (TextView) findViewById(R.id.text_view_resolution);
     }
 }
